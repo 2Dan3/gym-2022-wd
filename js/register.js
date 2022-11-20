cd21210b0a3656c90a4b7d60eeea0bd5ea1a860d
2Dan3 <dan23.ftn@gmail.com>
2022-11-20 20:25:58 +0100

    Add: register w/ alert msg

--- js/register.js
+++ js/register.js
@@ -10,68 +10,67 @@ document.body.appendChild(overlay);
 }
 
 
-var loginform = document.createElement('form');
-loginform.setAttribute('id', 'login-form');
-loginform.setAttribute('method', 'post');
-overlay.appendChild(loginform);
+var registerform = document.createElement('form');
+registerform.setAttribute('id', 'register-form');
+registerform.setAttribute('method', 'post');
+overlay.appendChild(registerform);
 
 var h1 = document.createElement('h1');
-h1.textContent = 'Sign in with your credentials:';
-loginform.appendChild(h1);
+h1.textContent = 'Sign up:';
+registerform.appendChild(h1);
 
 var username_field = document.createElement('input');
 username_field.setAttribute('name', 'username');
 username_field.setAttribute('type', 'text');
 username_field.setAttribute('placeholder', 'Username');
-loginform.appendChild(username_field);
+registerform.appendChild(username_field);
 
 var password_field = document.createElement('input');
 password_field.setAttribute('name', 'password');
 password_field.setAttribute('type', 'password');
 password_field.setAttribute('placeholder', 'Password');
-loginform.appendChild(password_field);
+registerform.appendChild(password_field);
 
-var login_btn = document.createElement('button');
-login_btn.onclick = do_login;
-login_btn.textContent = 'Sign in';
-loginform.appendChild(login_btn);
+var register_btn = document.createElement('button');
+register_btn.onclick = do_register;
+register_btn.textContent = 'Sign up';
+registerform.appendChild(register_btn);
 
-var p_reg = document.createElement('p');
-p_reg.classList.add('register-btn');
-p_reg.textContent = 'Not a member yet?';
-// p_reg.onclick = do_register;
-loginform.appendChild(p_reg);
+// var p_login = document.createElement('p');
+// p_login.classList.add('login-btn');
+// p_login.textContent = 'Already a member?';
+//     // p_reg.onclick = do_login;
+// registerform.appendChild(p_login);
 
 // *TODO: getElementsByClassName + map this event listener to each elem
-var login_buttons = document.getElementsByClassName('login-btn');
-[].forEach.call(login_buttons, (login_button_found) => {
-    login_button_found.addEventListener('click', (e) => { 
+var register_buttons = document.getElementsByClassName('register-btn'); 
+[].forEach.call(register_buttons, (register_button_found) => {
+    register_button_found.addEventListener('click', (e) => { 
         e.stopPropagation();
         // +
-        document.getElementById('register-form').style.display = 'none';
+        document.getElementById('login-form').style.display = 'none';
         overlay.style.display = 'block';
-        loginform.style.display = 'block';
+        registerform.style.display = 'block';
      });
     }
 );
 
-
 overlay.addEventListener('click', (e) => {
      overlay.style.display = 'none';
-     loginform.reset();
+     registerform.reset();
     //  +
-     loginform.style.display = 'none';
+     registerform.style.display = 'none';
  });
 
-loginform.addEventListener('click', (e) => { e.stopPropagation(); });
+registerform.addEventListener('click', (e) => { e.stopPropagation(); });
 
-function do_login(){
+function do_register(){
     // *TODO: Form validity check - error if empty or invalid
     // *TODO: Store logged info inside localStorage
-    alert("LOGIN request SENT");
+    alert("register request SENT");
 }
-// function show_login_form(){
-//     console.log('login showed');
+// function show_register_form(){
+//     console.log('register showed');
 //     overlay.style.display = 'block';
 //     setTimeout(() => {
 //         document.body.addEventListener('click', (e) => { overlay.style.display = 'none'; });

70da2b836e4325d85ab6dbfc9a30b19c7b43e89c
2Dan3 <dan23.ftn@gmail.com>
2022-12-12 13:36:02 +0100

    Add: homepage fitness centers load from Firebase

--- js/fitcenters.js
+++ js/fitcenters.js
@@ -1,4 +1,4 @@
-// var FC_URL = "https://jsonplaceholder.typicode.com/photos"
+const FC_URL = "https://fitnessandusers-default-rtdb.europe-west1.firebasedatabase.app"
 
 
 // Kada se ucita stranica ucitaju se svi FC sa servera
@@ -10,49 +10,58 @@ async function load_fitness_centers(){
     
     const response_array = await request_all_fitness_centers()
     // alert("Loading all fitness centers...");
-    setTimeout(() => {
-        finish_fitness_center_rendering(response_array);
-    }, 600);
+    // setTimeout(() => {
+        // finish_fitness_center_rendering(response_array);
+    // }, 600);
 
 }
 
-async function finish_fitness_center_rendering(fc_array){
+async function finish_fitness_center_rendering(response){
     // setTimeout(() => {
-        fc_array.map(makeFitCenterCard);
+        for (var id_key in response) {
+            var fc_obj = response[id_key];
+            // console.log(fc_obj);
+            makeFitCenterCard(fc_obj);
+        }
         // alert("Successfully rendered!");
         // }, 2000);
 }
 
 async function request_all_fitness_centers(){
 
-    const response_fitness_centers = [
-    {"fc_id":1, "fc_name":"Pumpin' Iron", "fc_address":"Neka Tamo 58a, Novi Sad, 21000", "fc_est":2006, "fc_pic":"../assets/gympic.png", "fc_tr_num":3, "fc_memb_price":2490, "fc_ratings_avg":4.5, "fc_ratings_num":2},
-    {"fc_id":2, "fc_name":"Leg-go!", "fc_address":"Trg Novih Ulica 11, Novi Sad, 21000", "fc_est":1998, "fc_pic":"../assets/gympic2.jpg", "fc_tr_num":2, "fc_memb_price":2099, "fc_ratings_avg":4.0, "fc_ratings_num":3},
-    {"fc_id":3, "fc_name":"Load", "fc_address":"Nova Ulicica 11, Novi Sad, 21000", "fc_est":2001, "fc_pic":"../assets/gympic3.png", "fc_tr_num":2, "fc_memb_price":2690, "fc_ratings_avg":4.0, "fc_ratings_num":3},
-    {"fc_id":4, "fc_name":"Armed&Ready", "fc_address":"Trg Novih Ulica 83, Novi Sad, 21000", "fc_est":2021, "fc_pic":"../assets/gympic4.png", "fc_tr_num":3, "fc_memb_price":2890, "fc_ratings_avg":5, "fc_ratings_num":1},
-    {"fc_id":5, "fc_name":'Drop \'em!', "fc_address":"Ulica Nova 27b, Novi Sad, 21000", "fc_est":2011, "fc_pic":"../assets/gympic5.jpg", "fc_tr_num":1, "fc_memb_price":1990, "fc_ratings_avg":4.5, "fc_ratings_num":2},
-    {"fc_id":6, "fc_name":"Walk the talk", "fc_address":"Ulica Stara 71, Novi Sad, 21000", "fc_est":2021, "fc_pic":"../assets/gympic6.png", "fc_tr_num":1, "fc_memb_price":1890, "fc_ratings_avg":3.67, "fc_ratings_num":3},
-    {"fc_id":7, "fc_name":"Testosterone", "fc_address":"Ulicica Starija 19a, Novi Sad, 21000", "fc_est":2022, "fc_pic":"../assets/gympic7.jpg", "fc_tr_num":2, "fc_memb_price":2790, "fc_ratings_avg":4.0, "fc_ratings_num":1},
-    {"fc_id":8, "fc_name":"Stronger than yesterday", "fc_address":"Ulicica Skorasnja 29, Novi Sad, 21000", "fc_est":2021, "fc_pic":"../assets/gympic8.png", "fc_tr_num":2, "fc_memb_price":2090, "fc_ratings_avg":4, "fc_ratings_num":1},
-    ];
+    var response_fitness_centers;
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if(this.readyState == 4) {
+            if(this.status == 200) {
+                response_fitness_centers = JSON.parse(req.responseText);
+                console.log(response_fitness_centers);
+                finish_fitness_center_rendering(response_fitness_centers);
+            }else {
+                console.error('Error loading Fitness Centers.')
+                return null
+            }
+        }
+    }
+
+    req.open('GET', FC_URL + '/fitnesCentri.json');
+    req.send();
+
+    // const response_fitness_centers = [
+    // {"fc_id":1, "fc_name":"Pumpin' Iron", "fc_address":"Neka Tamo 58a, Novi Sad, 21000", "fc_est":2006, "fc_pic":"../assets/gympic.png", "fc_tr_num":3, "fc_memb_price":2490, "fc_ratings_avg":4.5, "fc_ratings_num":2},
+    // {"fc_id":2, "fc_name":"Leg-go!", "fc_address":"Trg Novih Ulica 11, Novi Sad, 21000", "fc_est":1998, "fc_pic":"../assets/gympic2.jpg", "fc_tr_num":2, "fc_memb_price":2099, "fc_ratings_avg":4.0, "fc_ratings_num":3},
+    // {"fc_id":3, "fc_name":"Load", "fc_address":"Nova Ulicica 11, Novi Sad, 21000", "fc_est":2001, "fc_pic":"../assets/gympic3.png", "fc_tr_num":2, "fc_memb_price":2690, "fc_ratings_avg":4.0, "fc_ratings_num":3},
+    // {"fc_id":4, "fc_name":"Armed&Ready", "fc_address":"Trg Novih Ulica 83, Novi Sad, 21000", "fc_est":2021, "fc_pic":"../assets/gympic4.png", "fc_tr_num":3, "fc_memb_price":2890, "fc_ratings_avg":5, "fc_ratings_num":1},
+    // {"fc_id":5, "fc_name":'Drop \'em!', "fc_address":"Ulica Nova 27b, Novi Sad, 21000", "fc_est":2011, "fc_pic":"../assets/gympic5.jpg", "fc_tr_num":1, "fc_memb_price":1990, "fc_ratings_avg":4.5, "fc_ratings_num":2},
+    // {"fc_id":6, "fc_name":"Walk the talk", "fc_address":"Ulica Stara 71, Novi Sad, 21000", "fc_est":2021, "fc_pic":"../assets/gympic6.png", "fc_tr_num":1, "fc_memb_price":1890, "fc_ratings_avg":3.67, "fc_ratings_num":3},
+    // {"fc_id":7, "fc_name":"Testosterone", "fc_address":"Ulicica Starija 19a, Novi Sad, 21000", "fc_est":2022, "fc_pic":"../assets/gympic7.jpg", "fc_tr_num":2, "fc_memb_price":2790, "fc_ratings_avg":4.0, "fc_ratings_num":1},
+    // {"fc_id":8, "fc_name":"Stronger than yesterday", "fc_address":"Ulicica Skorasnja 29, Novi Sad, 21000", "fc_est":2021, "fc_pic":"../assets/gympic8.png", "fc_tr_num":2, "fc_memb_price":2090, "fc_ratings_avg":4, "fc_ratings_num":1},
+    // ];
 
     return response_fitness_centers;
 }
 
-// request.onreadystatechange = function() {
-    // if(this.readyState == 4) {
-    //     if(this.status == 200) {
-            // var fcs = JSON.parse(request.responseText);
-            // finish_fitness_center_rendering(fcs); 
-
-        // }else {
-        //     console.error('Error loading Fitness Centers.')
-        // }
-    // }
-// }
-
-// request.open('GET', FC_URL);
-// request.send();
 
 function makeFitCenterCard(fc){
     // setTimeout(()=>{}, 1000);
@@ -74,7 +83,7 @@ function makeFitCenterCard(fc){
     aElement.setAttribute('id', 'see-more-link');
     aElement.setAttribute('href', './fitcenter.html');
     // Alternatively (for older browsers) aElement.innerHtml
-    aElement.textContent = fc.fc_name;
+    aElement.textContent = fc.naziv;
     cardTopLeftDiv.appendChild(aElement);
     cardTopDiv.appendChild(cardTopLeftDiv);
     var cardTopRightDiv = document.createElement('div');
@@ -85,7 +94,7 @@ function makeFitCenterCard(fc){
     var cardMiddleDiv = document.createElement('div');
     cardMiddleDiv.classList.add('card-middle');
     var cardMiddlePic = document.createElement('img');
-    cardMiddlePic.setAttribute('src', fc.fc_pic);
+    cardMiddlePic.setAttribute('src', fc.slika);
     cardMiddleDiv.appendChild(cardMiddlePic);
     cardContentWrapper.appendChild(cardMiddleDiv);
 
@@ -103,7 +112,7 @@ function makeFitCenterCard(fc){
     cardBottomDiv.appendChild(cardBottomLeftDiv);
     var cardBottomRightDiv = document.createElement('div');
     cardBottomRightDiv.classList.add('card-bottom-right');
-    cardBottomRightDiv.textContent = fc.fc_ratings_avg.toFixed(1);
+    cardBottomRightDiv.textContent = fc.prosecnaOcena.toFixed(2);
     cardBottomDiv.appendChild(cardBottomRightDiv);
     cardContentWrapper.appendChild(cardBottomDiv);
 }

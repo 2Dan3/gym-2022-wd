bb619413d9c1173b5cf4e1a36b554c95a6f68c57
2Dan3 <dan23.ftn@gmail.com>
2023-01-13 18:39:44 +0100

    Add: register w/ Firebase

--- js/register.js
+++ js/register.js
@@ -13,6 +13,10 @@ document.body.appendChild(overlay);
 var registerform = document.createElement('form');
 registerform.setAttribute('id', 'register-form');
 registerform.setAttribute('method', 'post');
+registerform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    do_register();
+})
 overlay.appendChild(registerform);
 
 var h1 = document.createElement('h1');
@@ -24,6 +28,7 @@ username_field.setAttribute('name', 'username');
 username_field.setAttribute('type', 'text');
 username_field.setAttribute('class', 'register-field');
 username_field.setAttribute('placeholder', 'Username');
+username_field.setAttribute('id', 'reg-username-input');
 registerform.appendChild(username_field);
 
 var password_field = document.createElement('input');
@@ -31,6 +36,7 @@ password_field.setAttribute('name', 'password');
 password_field.setAttribute('type', 'password');
 password_field.setAttribute('class', 'register-field');
 password_field.setAttribute('placeholder', 'Password');
+password_field.setAttribute('id', 'reg-password-input');
 registerform.appendChild(password_field);
 
 var email_field = document.createElement('input');
@@ -38,6 +44,7 @@ email_field.setAttribute('name', 'email');
 email_field.setAttribute('type', 'email');
 email_field.setAttribute('class', 'register-field');
 email_field.setAttribute('placeholder', 'Email');
+email_field.setAttribute('id', 'reg-email-input');
 registerform.appendChild(email_field);
 
 var first_name_field = document.createElement('input');
@@ -45,6 +52,7 @@ first_name_field.setAttribute('name', 'name');
 first_name_field.setAttribute('type', 'text');
 first_name_field.setAttribute('class', 'register-field');
 first_name_field.setAttribute('placeholder', 'Name');
+first_name_field.setAttribute('id', 'reg-name-input');
 registerform.appendChild(first_name_field);
 
 var last_name_field = document.createElement('input');
@@ -52,13 +60,15 @@ last_name_field.setAttribute('name', 'surname');
 last_name_field.setAttribute('type', 'text');
 last_name_field.setAttribute('class', 'register-field');
 last_name_field.setAttribute('placeholder', 'Surname');
-registerform.appendChild(first_name_field);
+last_name_field.setAttribute('id', 'reg-surname-input');
+registerform.appendChild(last_name_field);
 
 var address_field = document.createElement('input');
 address_field.setAttribute('name', 'address');
 address_field.setAttribute('type', 'text');
 address_field.setAttribute('class', 'register-field');
 address_field.setAttribute('placeholder', 'Address');
+address_field.setAttribute('id', 'reg-address-input');
 registerform.appendChild(address_field);
 
 var phone_field = document.createElement('input');
@@ -66,6 +76,7 @@ phone_field.setAttribute('name', 'phone');
 phone_field.setAttribute('type', 'phone');
 phone_field.setAttribute('class', 'register-field');
 phone_field.setAttribute('placeholder', 'Phone Num.');
+phone_field.setAttribute('id', 'reg-phone-input');
 registerform.appendChild(phone_field);
 
 var birth_field = document.createElement('input');
@@ -73,10 +84,11 @@ birth_field.setAttribute('name', 'birth');
 birth_field.setAttribute('type', 'date');
 birth_field.setAttribute('class', 'register-field');
 birth_field.setAttribute('placeholder', 'Date of birth');
+birth_field.setAttribute('id', 'reg-birth-input');
 registerform.appendChild(birth_field);
 
 var register_btn = document.createElement('button');
-register_btn.onclick = do_register;
+register_btn.setAttribute('type', 'submit');
 register_btn.textContent = 'Sign up';
 registerform.appendChild(register_btn);
 
@@ -108,12 +120,54 @@ overlay.addEventListener('click', (e) => {
 
 registerform.addEventListener('click', (e) => { e.stopPropagation(); });
 
+
 function do_register(){
     if ( !isRegFormValid() ) {
         alert("Not all fields were filled!");
         return;
     }
+
+    let address = document.getElementById('reg-address-input').value.trim();
+    let birth = document.getElementById('reg-birth-input').value.toString();
+    let email = document.getElementById('reg-email-input').value;
+    let name = document.getElementById('reg-name-input').value.trim();
+    let pass = document.getElementById('reg-password-input').value;
+    let surname = document.getElementById('reg-surname-input').value.trim();
+    let phone = document.getElementById('reg-phone-input').value.toString();
+    let username = document.getElementById('reg-username-input').value.trim();
+    
+    new_user = 
+    {   
+        "adresa":address,
+        "datumRodjenja":birth,
+        "email":email,
+        "ime":name,
+        "lozinka":pass,
+        "prezime":surname,
+        "telefon":phone,
+        "korisnickoIme":username
+    };
+
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function() {
+        if(this.readyState == 4) {
+            if(this.status == 200) {
+                alert("Successfully registered!");
+            }else {
+                console.error('Error creating User.');
+                alert("An error occured while registering User.");
+            }
+            alert("check console & network...");
+            to_users_page();
+        }
+    }
+    // console.log("form obj: " + JSON.stringify(new_user));
     console.log("register requested");
+    req.open('POST',  'https://fitnessandusers-default-rtdb.europe-west1.firebasedatabase.app' + '/korisnici/.json');
+    req.send(JSON.stringify(new_user));
+
+    
 }
 
 function isRegFormValid(){
